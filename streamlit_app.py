4973d548ceba63d2736efc3185c3cf0bd03aeb72
DSPiyush <94290309+DSPiyush@users.noreply.github.com>
2023-11-05 15:00:04 +0530

    Update streamlit_app.py
    added multiselect picker object widget
    widget uses indices of the dataframe
    index column has been set to the fruit names for ease

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,15 @@ streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header("🍌🥭Build your own fruit smoothie🥝🍇")
 
+# reading the csv file and creating a dataframe to use it in our app
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#We are going to create a multi select (picker Object) for the users to chose their choice of fruit for the smoothie - it'll be like a widget
+#We need to change the index of the my_fruit_list dataframe to fruit column as they are unique and not null and thats what we are going to be seing in our widget
+
+my_fruit_list.set_index('Fruit', inplace = True)
+
+# After setting the index as fruit we are going to create the widget from multiselect and let the user pick the fruit, lets see how
+streamlit.multiselect(label = "Pick some fruits:", options = list(my_fruit_list.index)
+
 streamlit.dataframe(my_fruit_list)
