3611d0271fb68346d480948f74f2124d19d44945
DSPiyush <94290309+DSPiyush@users.noreply.github.com>
2023-11-07 17:00:49 +0530

    Update streamlit_app.py
    Checking if we can access a table's data

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,13 +50,20 @@ except requests.exceptions.JSONDecodeError:
   print("Waiting for you to enter a value...")
 
 # below code is for the connection from snowflake to streamlit, after we have made the changes in Streamlit app -> settings -> secrets 
+
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+first_row = my_cur.execute('select * from fruit_load_list')
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contains: ")
 streamlit.text(my_data_row)
-
 
 
 
