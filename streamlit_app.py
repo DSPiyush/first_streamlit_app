30ddfae580337ee3f05a811ed434dee5a0e43829
DSPiyush <94290309+DSPiyush@users.noreply.github.com>
2023-11-09 01:24:33 +0530

    Update streamlit_app.py
    Added another function to insert a row and a button
    The only problem is, we're still not adding the fruit name. We still have "from streamlit" hard-coded in.

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,30 +12,17 @@ streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
+
+#NEW SECTION
 streamlit.header("🍌🥭Build your own fruit smoothie🥝🍇")
-
-# reading the csv file and creating a dataframe to use it in our app
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-#We are going to create a multi select (picker Object) for the users to chose their choice of fruit for the smoothie - it'll be like a widget
-#We need to change the index of the my_fruit_list dataframe to fruit column as they are unique and not null and thats what we are going to be seing in our widget
-
 my_fruit_list.set_index('Fruit', inplace = True)
-
-# After setting the index as fruit we are going to create the widget from multiselect and let the user pick the fruit, lets see how
-#streamlit.multiselect(label = "Pick some fruits:", options = list(my_fruit_list.index), default=['Avocado','Strawberries'])
-
-#Now we need to make the table shown there a bit dynamic, limiting the rows to show only the selected fields by the user
-
 fruits_to_show = streamlit.multiselect(label = "Pick some fruits:", options = list(my_fruit_list.index), default=['Avocado','Strawberries'])
-
 streamlit.dataframe(my_fruit_list.loc[fruits_to_show])
 
 #NEW SECTION
 streamlit.header('Fruityvice Fruit Advice!')
 
-# fruit_choice = streamlit.text_input("For which fruit do you need help", 'Kiwi')
-#streamlit.write("The user Entered", fruit_choice) # This is removed 
 def get_fruityvice_data(this_fruit_choice):
     # Take your json response and normalize it , this is the same code we were using before
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
@@ -53,26 +40,14 @@ except URLError as e:
   streamlit.error()
 
 
-# below code is for the connection from snowflake to streamlit, after we have made the changes in Streamlit app -> settings -> secrets 
-
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-# my_data_row = my_cur.fetchone()
-# streamlit.text("Hello from Snowflake:")
-# streamlit.text(my_data_row)
-
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# first_row = my_cur.execute('select * from fruit_load_list')
-# my_data_rows = my_cur.fetchall()
+#NEW SECTION
 streamlit.header("The fruit load list contains: ")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute('select * from fruit_load_list')
         return my_cur.fetchall()
 
-#Add a button to load the fruit
+
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
@@ -81,14 +56,21 @@ if streamlit.button('Get Fruit Load List'):
 
 streamlit.stop()
 
-# adding a second text input box
 add_my_fruit = streamlit.text_input(label = 'What Fruit would you like to add ?')
 streamlit.write('Thanks for adding '+add_my_fruit)
 
 my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
 
 
-
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
+        return "Thanks for adding" + new_fruit 
+        
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
 
 
 
